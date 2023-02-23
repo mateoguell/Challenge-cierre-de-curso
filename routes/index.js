74b6e29b125581d1128f1bb2f3094568c1b15652
mateoguell <m4t30guell@gmail.com>
2023-02-22 21:24:30 -0300

    aplicando express generator

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+//Requerimos el controlador
+const mainController = require('../controllers/mainController');
+
+router.get('/', mainController.index);
 
 module.exports = router;
